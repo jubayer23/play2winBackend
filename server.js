7e6d16fabfa58f72b4b8ce19d13b30e4692c5ff0
akhil kaparthi <akhilkaparthi@akhils-MacBook-Pro.local>
2020-10-22 13:00:17 +0100

    code added for cors in server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var path = require('path');
 
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(session({
     cookie: { maxAge: 60000 },
     store: new session.MemoryStore,
