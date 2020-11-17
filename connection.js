560959cb88e201e060015a72137e252deb437623
akhilkaparthi <akhileshkaparthi@gmail.com>
2020-11-17 20:33:55 +0100

    changes done to add mysql sprytech credentials

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 var mysqlConnection = mysql.createConnection({
-    host : "test.chdzjulmvj4i.eu-west-2.rds.amazonaws.com",
-    user : "root",
+    host : "test.cqyfksx8uik6.eu-west-1.rds.amazonaws.com",
+    user : "admin",
     password: "zsXrIwCYy0%b",
     database: "test",
     multipleStatements : true
